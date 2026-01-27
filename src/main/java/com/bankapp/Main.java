c2ad919be522857b09ea43165bab262af5cf7d2e
The4light <ddogbonna08@gmail.com>
2026-01-27 23:11:46 +0100

    more frontend folder structure addition Javafx integration build of test  login page

--- src/main/java/com/bankapp/Main.java
+++ src/main/java/com/bankapp/Main.java
@@ -1,5 +1,32 @@
-public class Main {
+package com.bankapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        // attach stylesheet
+        scene.getStylesheets().add(
+                getClass().getResource("/css/styles.css").toExternalForm()
+        );
+
+        stage.setTitle("Online Banking System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 }
